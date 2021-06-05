85a10703b150e7760311183aeb018b76b8776060
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-05 14:03:24 -0400

    Initial commit

---
+++ src/main/java/base/app.java
@@ -0,0 +1,10 @@
+package base;
+/*Create a program that prompts for your name and prints a greeting using your name.
+
+ */
+public class app {
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+
+    }
+}
